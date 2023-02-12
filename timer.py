855bcd467a2c413464b279ce23d8d4ec577469ab
Ali Yahya <alive@google.com>
2023-02-12 14:28:13 -0800

    remove systems time tracker

--- timer.py
+++ timer.py
@@ -45,7 +45,7 @@ COLUMN_MARGIN = 5
 
 # We currently assume that each day column is immediately followed
 # by week, month, and quarter columns.
-DAY_COLUMN_INDICES = [2, 6, 10, 14, 18]
+DAY_COLUMN_INDICES = [2, 6, 10, 14]
 
 FOCUS_UNIT_DURATION = 1800  # Seconds (30 minutes).
 BASE_INTERRUPTION_PENALTY = 0.04 # Time units
